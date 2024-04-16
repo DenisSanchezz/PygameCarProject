79119ce4cf3fb0b7c33c6369b1b4dc58fede885e
Braeden Boyce <156366121+braeden-boyce@users.noreply.github.com>
2024-04-16 14:03:35 -0500

    finished the basic menu design

--- Car Game/CarGame.py
+++ Car Game/CarGame.py
@@ -1,6 +1,6 @@
 import pygame
 
-def menu(screen, menuFont, menuText, playText, creditsText, quitText, textColor):
+def menu(screen, menuFont, menuText, playText, creditsText, quitText, whiteTextColor, blackTextColor):
     #bg
     screen.fill('0x737373')
     
@@ -15,12 +15,22 @@ def menu(screen, menuFont, menuText, playText, creditsText, quitText, textColor)
     pygame.draw.rect(screen, "0xFFFFFF", [100, 225, 300, 75], 100)
     pygame.draw.rect(screen, "0x00FF00", [100, 225, 300, 75], 10)
     
+    pygame.draw.rect(screen, "0xFFFFFF", [100, 325, 300, 75], 100)
+    pygame.draw.rect(screen, "0xFFFF00", [100, 325, 300, 75], 10)
+    
+    pygame.draw.rect(screen, "0xFFFFFF", [100, 425, 300, 75], 100)
+    pygame.draw.rect(screen, "0xFF0000", [100, 425, 300, 75], 10)
+    
     #render the text
-    mainMenuBannerText = menuFont.render(menuText, True, textColor)
-    playButtonText = menuFont.render(playText, True, textColor)
+    mainMenuBannerText = menuFont.render(menuText, True, whiteTextColor)
+    playButtonText = menuFont.render(playText, True, blackTextColor)
+    creditsButtonText = menuFont.render(creditsText, True, blackTextColor)
+    quitButtonText = menuFont.render(quitText, True, blackTextColor)
+    
     screen.blit(mainMenuBannerText, (150, 60))
-    
-    
+    screen.blit(playButtonText, (200, 240))
+    screen.blit(creditsButtonText, (165, 340))
+    screen.blit(quitButtonText, (200, 440))
 
 def main():
     # init()
@@ -34,7 +44,8 @@ def main():
 
     # setup the menu variables
     menuFont = pygame.font.SysFont("Consolas", 40, bold=True)
-    textColor = ('0xFFFFFF')
+    whiteTextColor = ('0xFFFFFF')
+    blackTextColor = ('0x000000')
     
     menuText = "Main Menu"
     playText = "PLAY"
@@ -55,7 +66,7 @@ def main():
                 running = False
 
         if not gameActive and menuActive:
-            menu(screen, menuFont, menuText, textColor)
+            menu(screen, menuFont, menuText, playText, creditsText, quitText, whiteTextColor, blackTextColor)
 
         # Update display
         pygame.display.update()
