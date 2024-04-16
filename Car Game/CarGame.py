02e281f987b45bf2a1f9a8b744003a4dd92bb1e5
char1espope <156366283+char1espope@users.noreply.github.com>
2024-04-16 14:31:36 -0500

    Update CarGame.py
    different background

--- Car Game/CarGame.py
+++ Car Game/CarGame.py
@@ -1,21 +1,26 @@
+import os.path
 import pygame
+from sys import exit
+from random import randint
+
+scriptDir = os.path.dirname(os.path.abspath(__file__))
 
 def menu(screen, menuFont, menuText, textColor):
     mainMenuBannerText = menuFont.render(menuText, True, textColor)
-    screen.blit(mainMenuBannerText, (150, 40))
+    screen.blit(mainMenuBannerText, (600, 220))
 
 def main():
     # init()
     pygame.init()
 
     # setup the screen
-    screen_width = 500
-    screen_height = 600
+    screen_width = 1400
+    screen_height = 1000
     screen = pygame.display.set_mode((screen_width, screen_height))
     pygame.display.set_caption('The Swift and the Sulky')
 
     # setup the menu variables
-    menuFont = pygame.font.SysFont("Consolas", 40)
+    menuFont = pygame.font.SysFont("Modern", 40)
     textColor = (255, 255, 255)
     menuText = "Main Menu"
 
@@ -25,6 +30,8 @@ def main():
     running = True
     game_active = False
 
+    startscreen = pygame.image.load(os.path.join(scriptDir,"car game graphics/","titlescreen.png")).convert_alpha()
+    screen.blit(startscreen,(0,0))
     # Loop
     while running:
         for event in pygame.event.get():
